46d0b69ec9d88c279ba19f5a6cca2319c3b25f8d
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2020-05-05 19:18:46 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      {console.log("blad")}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
