0ed8419f6825702e685c841e847e96c16b8e7ca4
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2020-05-17 17:25:29 +0200

    signup with email and pass

--- src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
+++ src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SignIn from "../../components/sing-in/sing-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 import "./sing-in-and-sing-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 );
 
